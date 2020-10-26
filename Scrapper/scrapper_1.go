56ba032356671e4ab562463eefd0510c927e9a96
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2020-10-26 22:26:00 +0000

    MOD:

--- Scrapper/scrapper_1.go
+++ Scrapper/scrapper_1.go
@@ -22,3 +22,23 @@ func ScrapeContent_2() {
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Accept", "text/javascript, text/html, application/xml, text/xml, */*")
 	req.Header.Set("X-Prototype-Version", "1.6.0.2")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36")
+	req.Header.Set("Content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("Origin", "https://www.icharts.in")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://www.icharts.in/screener-eod.html")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9,te-IN;q=0.8,te;q=0.7,en-IN;q=0.6")
+	req.Header.Set("Cookie", "__auc=de511acc174ab5077e9fdc45c58; __gads=ID=6614628d09480cf1:T=1600602042:S=ALNI_Ma7uQM537PiSjHCc-HJNkMjuCvB_g; __utmz=192083122.1600621783.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmc=192083122; PHPSESSID=8qim5nkje9947lgthl6ttggc35; 0067889d30b7196cce14886855b1891a=061d2caf23fb6a3e57eccf014ec54d52; __asc=5ab10303174b133e279579d4f63; __utma=192083122.261844769.1600602012.1600696007.1600700802.6; __utmt=1; __utmb=192083122.1.10.1600700802")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
+}
