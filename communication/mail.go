690ca391fe000d1851c1e1ef8e86ee67585cf952
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2020-10-30 12:14:04 +0000

    ext4: make fsync to sync parent dir in no-journal for real this time

--- communication/mail.go
+++ communication/mail.go
@@ -15,3 +15,24 @@ func SendMail(c *types.Config) error {
 
 	from := c.Mail.From
 	password := c.Mail.Pass
+
+	// toList is list of email address that email is to be sent.
+	toList := c.Mail.To
+
+	// host is address of server that the
+	// sender's email address belongs,
+	// in this case its gmail smtp.gmail.com.
+	// For e.g if your are using yahoo
+	// mail change the address as smtp.mail.yahoo.com
+	host := c.Mail.Host
+
+	// 587 is the default port of smtp server
+	port := c.Mail.Port
+
+	// PlainAuth uses the given username and password to
+	// authenticate to host and act as identity.
+	// Usually identity should be the empty string,
+	// to act as username.
+	auth := smtp.PlainAuth("", from, password, host)
+
+	// SendMail uses TLS connection to send the mail
