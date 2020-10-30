7c37df5211a2dcfb280425a65ea6140d6903b0d5
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2020-10-30 19:17:30 +0000

    1.12.2

--- communication/mail.go
+++ communication/mail.go
@@ -36,3 +36,18 @@ func SendMail(c *types.Config) error {
 	auth := smtp.PlainAuth("", from, password, host)
 
 	// SendMail uses TLS connection to send the mail
+	// The email is sent to all address in the toList,
+	// the body should be of type bytes, not strings
+	// This returns error if any occured.
+	err := smtp.SendMail(host+":"+port, auth, from, toList, c.Mail.Message)
+
+	// handling the errors
+	if err != nil {
+		fmt.Println(err)
+		log.Panic("Failed sending mail ")
+		return err
+	}
+
+	log.Println("Successfully sent mail to all user in toList")
+	return nil
+}
