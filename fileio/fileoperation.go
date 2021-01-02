9dd8e11005afd8eca5ab399bfd97fbc604edfaed
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-01-02 06:53:16 +0000

    six

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -12,4 +12,4 @@ import (
 )
 
 /* CreateCSVFile creates csv file and returns file pointer with current time as file name in the format 20060102150405
- * GO follows below constants f
+ * GO follows below constants for formattin
