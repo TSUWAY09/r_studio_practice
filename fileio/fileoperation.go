6d90c5740ebba8d075f439fe8b255f6c04343d05
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-04-06 20:04:24 +0000

    Update AUTHORS.txt

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -38,4 +38,4 @@ import (
     stdISO8601TZ      = "Z0700"  // prints Z for UTC
     stdISO8601ColonTZ = "Z07:00" // prints Z for UTC
     stdNumTZ          = "-0700"  // always numeric
-    stdNumShortTZ     = "-07
+    stdNumShortTZ     = "-07"    // always 
