69b6d194c2752d516ba289879ff09049867eb7c5
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-04-16 13:49:52 +0000

    ignore tmp

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -41,4 +41,4 @@ import (
     stdNumShortTZ     = "-07"    // always numeric
     stdNumColonTZ     = "-07:00" // always numeric
 )
-* stockScannerType input parameter to
+* stockScannerType input parameter to read whether sto
