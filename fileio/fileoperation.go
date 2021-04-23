c7a839802b252acee71f2bc2105dd6e1398da953
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-04-23 09:15:24 +0000

    not used anymore

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -43,4 +43,4 @@ import (
 )
 * stockScannerType input parameter to read whether stock scanning option selected is bullish or bearish
 */
-func CreateCSVFile(stockSc
+func CreateCSVFile(stockScannerType int)
