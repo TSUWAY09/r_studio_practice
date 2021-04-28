18d38ed6ffa85c1e5159798951a738f7c35847ee
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-04-28 09:44:47 +0000

    Update javaIndex.html

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -43,4 +43,5 @@ import (
 )
 * stockScannerType input parameter to read whether stock scanning option selected is bullish or bearish
 */
-func CreateCSVFile(stockScannerType int) (*os.File, erro
+func CreateCSVFile(stockScannerType int) (*os.File, error) {
+	log.Println("
