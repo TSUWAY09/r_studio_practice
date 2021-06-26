aa4e2fca8b68d0c61458572ff9b55c0c300178c9
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-06-26 12:50:31 +0000

    Remove console.log

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -54,4 +54,5 @@ func CreateCSVFile(stockScannerType int) (*os.File, error) {
 		fileType = "Bearish_"
 	}
 	path := "/home/bhanureddy/Documents/" + fileType + timeValue + ".csv"
-	if err := os.MkdirAll(filepath.Dir(pat
+	if err := os.MkdirAll(filepath.Dir(path), 0777); err != nil {
+	
