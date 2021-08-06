55b875b5b6dec6985484a49493cb41bce130ec6f
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-08-06 09:41:12 +0000

    lecture8: updates

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -68,4 +68,4 @@ func CreateCSVFile(stockScannerType int) (*os.File, error) {
 }
 
 /*
-* WriteCSVFile writes to csv file with given strin
+* WriteCSVFile writes to csv file with given string slice as 
