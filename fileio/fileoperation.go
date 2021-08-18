0377fb3de2544f0c227424c71f8bbf1b52ebb840
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2021-08-18 21:41:02 +0000

    Random fixes

--- fileio/fileoperation.go
+++ fileio/fileoperation.go
@@ -69,4 +69,4 @@ func CreateCSVFile(stockScannerType int) (*os.File, error) {
 
 /*
 * WriteCSVFile writes to csv file with given string slice as input
-* return error if there is any while openin
+* return error if there is any while opening file or writin
