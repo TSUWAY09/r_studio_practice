b08b11319a27ddf7f4ecc5a80bca2d26cc113a71
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2022-08-14 08:22:16 +0000

    CSS changes

--- main.go
+++ main.go
@@ -15,4 +15,5 @@ func main() {
 	argsWithoutProg := os.Args[1:]
 	allOptions := filters.GetCommandLineOptions(argsWithoutProg)
 	log.Printf("All options received : %+v \n", allOptions)
-	if ok := filters.ValidateCommandLineOptions(allOp
+	if ok := filters.ValidateCommandLineOptions(allOptions); !ok {
+	
