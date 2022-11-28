afd89587a2dcc1752543af92ac4ce28f80a9f11b
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2022-11-28 16:46:59 +0000

    no longer required

--- requests/requesthandlerr.go
+++ requests/requesthandlerr.go
@@ -18,4 +18,5 @@ func CreateAPIRequestAndGetResponse(value string, client *http.Client) (error, s
 	if err != nil {
 		log.Fatal(err)
 	}
-	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.1
+	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36")
+	requ
