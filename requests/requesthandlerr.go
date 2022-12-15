b74417200f8f268faf45833b2fe3f1f2e379ddfa
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2022-12-15 10:58:33 +0000

    tag structure

--- requests/requesthandlerr.go
+++ requests/requesthandlerr.go
@@ -21,4 +21,5 @@ func CreateAPIRequestAndGetResponse(value string, client *http.Client) (error, s
 	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36")
 	request.Header.Set("Accept-Language", "en-IN")
 	request.Header.Set("Sec-Fetch-Site", "same-origin")
-	request.Header.Set("Sec-Fetch-M
+	request.Header.Set("Sec-Fetch-Mode", "cors")
+	requ
