866038e5718054d3bd7cca886245fbca3be7224c
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2022-12-15 19:57:02 +0000

    Fixed tomahawk classpath entry

--- requests/requesthandlerr.go
+++ requests/requesthandlerr.go
@@ -22,4 +22,4 @@ func CreateAPIRequestAndGetResponse(value string, client *http.Client) (error, s
 	request.Header.Set("Accept-Language", "en-IN")
 	request.Header.Set("Sec-Fetch-Site", "same-origin")
 	request.Header.Set("Sec-Fetch-Mode", "cors")
-	requ
+	request.Header.Set("Sec
