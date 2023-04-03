6357e6905a40d567dc7e520aeb27ca63d5f23492
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2023-04-03 09:18:15 +0000

    freshen

--- types/dataTypes.go
+++ types/dataTypes.go
@@ -4,3 +4,21 @@ package types
 
 import (
 	"gopkg.in/yaml.v2"
+)
+
+// used for determining whether a stock selected is Bearish or Bullish
+const (
+	DUMMY = iota
+	BULLISH
+	BEARISH
+)
+
+type Params struct {
+	Key   string
+	Value Value
+}
+
+type filter string
+
+type Value struct {
+	Action string
