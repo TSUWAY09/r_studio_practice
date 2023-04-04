622176b6f4e7726d92aeecaeb1e3aa20f52fda1d
TSUWAY09 <93443765+TSUWAY09@users.noreply.github.com>
2023-04-04 12:15:43 +0000

    New translations template.pot (French)

--- types/dataTypes.go
+++ types/dataTypes.go
@@ -22,3 +22,24 @@ type filter string
 
 type Value struct {
 	Action string
+	Info   map[string]map[filter]string
+}
+
+type Config struct {
+	Mail Mail `yaml:"mail"`
+}
+
+type Mail struct {
+	From    string   `yaml:"from"`
+	To      []string `yaml:"to"`
+	Pass    string   `yaml:"pass"`
+	Host    string   `yaml:"host"`
+	Port    string   `yaml:"port"`
+	Message []byte   `yaml:"-"`
+}
+
+func (c *Config) Parse(data []byte) error {
+	return yaml.Unmarshal(data, c)
+}
+
+//InfoParams := make(map[string]map[filter]string)
